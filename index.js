2a1644f9eb99fccc7754e3cc1532a827cd062fad
Lucas Galhardo <lucasrosagalhardo@gmail.com>
2024-09-14 18:05:38 -0300

    Iniciado menu da aplicação

--- index.js
+++ index.js
@@ -1,6 +1,42 @@
-const Start = () =>
+const { select } = require('@inquirer/prompts');
+
+const Start = async () =>
 {
-    console.log('começou');
+    while (true) 
+    {
+        const opcao = await select({
+            message: "Menu >",
+            choices: 
+            [
+                {
+                    name: "Cadastrar meta",
+                    value: "cadastrar"                    
+                },
+                {
+                    name: "Listar metas",
+                    value: "listar"
+                },
+                {
+                    name: "Sair",
+                    value: "sair"
+                }
+            ]
+        });
+
+        switch (opcao)
+        {
+            case "cadastrar":
+                console.log('Cadastrando meta...');
+                break;
+            case "listar":
+                console.log('Listando metas...');
+                break;        
+            default:
+            case "sair":    
+                console.log('Saindo...');
+                return;
+        }
+    }
 }
 
 Start();
